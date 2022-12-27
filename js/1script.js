4513233786d5a24663d6a42b40bcf81a294520c6
Денис Михальчук <denismykhalchukofficial@gmail.com>
2022-12-27 13:36:50 +0200

    Homework5

---
+++ js/1script.js
@@ -0,0 +1,9 @@
+let x=+prompt('Enter the first number :');
+let y=+prompt('Enter the second number :');
+let z=+prompt('Enter the third number :');
+let n=(x + y + z) / 3;
+
+alert(`The arithmetical mean by your numbers is :
+${n}
+Calculated by the formula :
+(${x} + ${y} + ${z}) / 3 =${n}`)
